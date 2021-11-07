3db82a0e04e8eb753d54882aa9eb2d3c4fd13b9a
alireza <babaie774@gmail.com>
2021-11-07 07:32:03 +0330

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,17 @@
-import React, { useState } from 'react'
-import TodoContext from './Component/Contexts/TodoContext'
-import HandleStatusContext from './Component/Contexts/HandleStatusContext'
-import InputTodoContext from './Component/Contexts/InputTodoContext'
+import React, { Provider } from 'react'
 import TodoList from './Component/TodoList'
 import './Style/Style.css'
+import TodoReducer from './Component/Reducers/TodoReducer'
+import TodoContext from './Component/Contexts/TodoContext'
 
 export default function App() {
 
 
-
-  const todo: any = [
-    { id: 1, content: "task 1", status: "completed" },
-    { id: 2, content: "task 2", status: "active" },
-  ]
-
-  const [todoes, setTodoes] = useState(todo)
-  const [filter, setFilter] = useState("active");
-
-
-  const HandleNewTodo = (input: any) => {
-    const { input: input2 } = input;
-    setTodoes([...todoes, { id: todoes.length + 1, content: input2, status: "active" }])
-  }
-
-
-
-
-  const handleStatusTodo = (id: number) => {
-    const newTodo = [...todoes]
-    newTodo.map((item: any) => {
-      if (item.id === id) {
-        item.status = item.status === 'active' ? 'completed' : 'active'
-      }
-      return newTodo
-    })
-    setTodoes(newTodo)
-  }
-
-
-  const handleFilterValue = (filter: string) => {
-    setFilter(filter);
-  };
-
   return (
-    <TodoContext.Provider value={todoes} >
-      <InputTodoContext.Provider value={{ InputTodo: HandleNewTodo }} >
-        <HandleStatusContext.Provider value={{ StatusTodo: handleStatusTodo }} >
-          <div>
-            <TodoList handleFilterValue={handleFilterValue} filter={filter} />
-          </div >
-        </HandleStatusContext.Provider>
-      </InputTodoContext.Provider>
-    </TodoContext.Provider >
+    <div>
+      <TodoContext.Provider value={TodoReducer as any} >
+        <TodoList />
+      </TodoContext.Provider >
+    </div >
   )
 }
--- src/Component/Services/HandleApi.tsx
+++ src/Component/Services/HandleApi.tsx
@@ -1,29 +1,21 @@
 import axios from 'axios';
 import React from 'react'
 
-export default function HandleApi() {
-    const getData = () => {
-        return axios.get('')
-            .then(response => response.data)
-            .then(data => data)
-    }
-
-    const postData = (data: any) => {
-        return axios.post('https://jsonplaceholder.typicode.com/todos', data).then(response => response.data).then(data => data)
-    }
-
-    const putData = (data: any) => {
-        return axios.put('https://jsonplaceholder.typicode.com/todos/1', data).then(response => response.data).then(data => data)
-    }
-
-    const deleteData = () => {
-        return axios.delete('https://jsonplaceholder.typicode.com/todos/1').then(response => response.data).then(data => data)
-    }
-
-    return {
-        getData,
-        postData,
-        putData,
-        deleteData
-    }
+const getData = async () => {
+    const { data } = await axios.get('http://localhost:3004/todoes')
+    return data
 }
+
+const postData = (data: any) => {
+    return axios.post('https://jsonplaceholder.typicode.com/todos', data).then(response => response.data).then(data => data)
+}
+
+const putData = (data: any) => {
+    return axios.put('https://jsonplaceholder.typicode.com/todos/1', data).then(response => response.data).then(data => data)
+}
+
+const deleteData = () => {
+    return axios.delete('https://jsonplaceholder.typicode.com/todos/1').then(response => response.data).then(data => data)
+}
+
+export { getData, postData, putData, deleteData }
